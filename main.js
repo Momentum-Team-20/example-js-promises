4f1e74167161ff9db4486c727b27ec3a12e9f070
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2023-10-25 15:12:53 -0400

    att catch statement

--- main.js
+++ main.js
@@ -7,11 +7,13 @@ let zipUrl = 'https://api.zippopotam.us/us/'
 zipForm.addEventListener('submit', (event) => {
     event.preventDefault();
     let zip = zipField.value
-    fetch(zipUrl + zip)
+    fetch('broken' + zipUrl + zip)
         .then((response) => {
             return response.json()
         }).then((parsedJsonResponse) => {
             resultsDiv.innerText = `Your place is: ${parsedJsonResponse['places'][0]['place name']}`
-})
+        }).catch(
+            alert('API call not successful 👎🏻')
+)
 })
 
